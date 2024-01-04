b8ec344b22b5cd7e41618c81c4be5b0698297939
Maxerder <baftalobididiuu@yahoo.com>
2024-01-04 11:10:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#from tkinter import *
+import tkinter as tk
+
+calculation= ""
+
+print("MERGE PRINT")
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+
+root = tk.Tk()#create the object
+root.geometry("400x500") #lenght x height
+root.title("Calculator")
+root.mainloop() #run the main loop
